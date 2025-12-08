89abf062a019fe6e03103aaa0fbbbdc35d6a9fc1
mathi <mathiasepc@msn.com>
2025-12-08 11:17:20 +0100

    Login works

--- src/main/java/org/example/gameconnectbackend/config/SecurityConfig.java
+++ src/main/java/org/example/gameconnectbackend/config/SecurityConfig.java
@@ -1,12 +1,18 @@
 package org.example.gameconnectbackend.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,20 +25,40 @@ import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+    private final UserDetailsService userDetailsService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    // Configure our HttpClient
     @Bean
-    public HttpClient igdbHttpClient() {
+    public HttpClient HttpClient() {
         return HttpClient.newBuilder()
                 // Sets a timeout for the request.
                 .connectTimeout(Duration.ofSeconds(50))
+                // which HttpClient to use
                 .version(HttpClient.Version.HTTP_2)
                 .build();
+    }
+
+    // This bean is used to authenticate the user
+    // It handles the authentication logic (login, logout, etc.)
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        var provider = new DaoAuthenticationProvider(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    // This bean is used to get the authentication manager
+    // It is used by Spring Security to authenticate the user
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) {
+        return configuration.getAuthenticationManager();
     }
 
 
@@ -47,7 +73,7 @@ public class SecurityConfig {
                 // ------------------------------------------
                 // !!! Here we configure our HttpSecurity !!!
                 .authorizeHttpRequests(c -> c
-                        .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
                         .anyRequest().permitAll()
                 )
                 // -------------------------------------------
